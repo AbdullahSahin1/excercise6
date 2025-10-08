500f990e5ae07876c51ef4ddf1946fbb6ede86be
AbdullahSahin1 <sahinabdullah2003@hotmail.com>
2025-10-08 13:49:37 +0300

    Arayüz

---
+++ app/src/main/java/com/example/excercise6/service/CryptoAPI.kt
@@ -0,0 +1,13 @@
+package com.example.excercise6.service
+
+
+import com.example.excercise6.model.CryptoModel
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface CryptoAPI {
+    //https://v6.exchangerate-api.com/v6/
+    // 8033422ecd3ea995431cf214/latest/USD
+    @GET("8033422ecd3ea995431cf214/latest/USD")
+    fun getData(): Call<List<CryptoModel>>
+}
